1ed6462d849b1258d84c58a8caa0813bf30fd405
Suson Waiba <susonwaiba@gmail.com>
2024-01-23 19:23:46 +0545

    Nix home-manager cleanup

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -1,54 +1,75 @@
 { config, pkgs, ... }:
 
-let
-	name = "Suson Waiba";
-	email = "susonwaiba@gmail.com";
-	githubUsername = "susonwaiba";
-in {
-	programs = {
-		# home-manager = {
-		# 	enable = true;
-		# };
-		bash = {
-			enable = true;
-			bashrcExtra = ''
-				export NIXPKGS_ALLOW_UNFREE=1
-				if [ -f ~/.my-bashrc ]; then
-					. ~/.my-bashrc
-				fi
-			'';
-			shellAliases = {
-				myName="echo ${name}";
-				myEmail="echo ${email}";
-				myWebSiteUrl="echo https://susonwaiba.github.io";
-				myGithubUrl="echo https://github.com/susonwaiba";
-			};
-		};
-		# zsh = {
-		# 	enable = true;
-		# };
-		git = {
-			enable = true;
-			userName = "${name}";
-			userEmail = "${email}";
-		};
-		direnv = {
-			enable = true;
-			nix-direnv = {
-				enable = true;
-			};
-		};
-	};
+{
+  home.username = "suson";
+  home.homeDirectory = if pkgs.system == "x86_64-linux" then "/home/suson" else "/Users/suson";
 
-	services = {
-	};
-	
-	home = {
-		username = "suson";
-		# homeDirectory = "/home/suson";
-		homeDirectory = "/Users/suson"; # For MacOS
-		stateVersion = "23.05";
-		packages = [
-		];
-	};
+  home.stateVersion = "23.05";
+
+  # Install via: nix-env -iA nixpkgs.home-manager
+  # programs.home-manager.enable = true;
+  programs.bash = {
+    enable = true;
+    bashrcExtra = ''
+      export NIXPKGS_ALLOW_UNFREE=1
+      if [ -f ~/.my-bashrc ]; then
+        . ~/.my-bashrc
+      fi
+    '';
+  };
+  programs.git = {
+    enable = true;
+    userName = "Suson Waiba";
+    userEmail = "susonwaiba@gmail.com";
+  };
+  programs.direnv = {
+    enable = true;
+    nix-direnv = {
+      enable = true;
+    };
+  };
+
+  home.packages = [
+    pkgs.htop
+    pkgs.btop
+    pkgs.neofetch
+    pkgs.nettools
+    pkgs.bat
+    pkgs.wrk
+    pkgs.tmux
+    pkgs.just
+    pkgs.gnumake
+    pkgs.sd
+    pkgs.gcc
+    pkgs.fzf
+    pkgs.fd
+    pkgs.jq
+    pkgs.xclip
+    pkgs.neovim
+    pkgs.lazygit
+    pkgs.sqlite
+    pkgs.pass
+    pkgs.nodejs_20
+    pkgs.yarn
+    pkgs.bun
+    pkgs.go
+    pkgs.rustup
+    pkgs.php83
+    pkgs.php83Packages.composer
+    pkgs.php83Packages.php-cs-fixer
+    pkgs.php83Packages.phpmd
+    pkgs.php83Extensions.pdo
+    pkgs.php83Extensions.mysqli
+    pkgs.php83Extensions.intl
+    pkgs.php83Extensions.iconv
+    pkgs.php83Extensions.sodium
+    pkgs.php83Extensions.mbstring
+    pkgs.php83Extensions.simplexml
+    pkgs.php83Extensions.gd
+    pkgs.php83Extensions.sqlite3
+    pkgs.php83Extensions.redis
+    pkgs.php83Extensions.mongodb
+    pkgs.php83Extensions.curl
+    pkgs.php83Extensions.zip
+  ];
 }
