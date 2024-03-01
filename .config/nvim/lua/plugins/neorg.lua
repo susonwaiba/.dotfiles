7a82f340e33296a0a3a1cb505b690399c656a0db
Suson Waiba <susonwaiba@gmail.com>
2024-03-01 18:05:01 +0545

    Updated nvim

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -1,9 +1,11 @@
 return {
     {
         'nvim-neorg/neorg',
-        ft = "norg",
+        dependencies = { 'nvim-lua/plenary.nvim' },
         build = ':Neorg sync-parsers',
-        dependencies = { { 'nvim-lua/plenary.nvim' } },
+        lazy = true,   -- enable lazy load
+        ft = "norg",   -- lazy load on file type
+        cmd = "Neorg", -- lazy load on command
         opts = {
             load = {
                 ['core.defaults'] = {},
@@ -37,7 +39,8 @@ return {
             vim.keymap.set("n", "<leader>nj", "<cmd>:Neorg journal<cr>", { desc = "Neorg journal" })
 
             vim.keymap.set("n", "<leader>nmn", "<cmd>:Neorg mode norg<cr>", { desc = "Neorg mode norg" })
-            vim.keymap.set("n", "<leader>nmh", "<cmd>:Neorg mode traverse-heading<cr>", { desc = "Neorg mode traverse-heading" })
+            vim.keymap.set("n", "<leader>nmh", "<cmd>:Neorg mode traverse-heading<cr>",
+                { desc = "Neorg mode traverse-heading" })
 
             vim.keymap.set("n", "<leader>nf", "<cmd>Telescope find_files cwd=~/.my-notes/<cr>", { desc = "Find notes" })
             vim.keymap.set("n", "<leader>ng", "<cmd>Telescope live_grep cwd=~/.my-notes/<cr>", { desc = "Grep note" })
--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -15,6 +15,10 @@ return {
         lazy = false,    -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
+            require('tokyonight').setup {
+                style = 'night',
+                transparent = true,
+            }
             -- Load the colorscheme here
             vim.cmd.colorscheme 'tokyonight-night'
 
@@ -22,23 +26,23 @@ return {
             vim.cmd.hi 'Comment gui=none'
         end,
     },
-    {
-        'xiyaowong/transparent.nvim',
-        priority = 999,
-        config = function()
-            require('transparent').setup({
-                groups = { -- table: default groups
-                    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
-                    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
-                    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
-                    'SignColumn', 'CursorLineNr', 'EndOfBuffer',
-                },
-                extra_groups = {
-                    "NormalFloat",   -- plugins which have float panel such as Lazy, Mason, LspInfo
-                    "NvimTreeNormal" -- NvimTree
-                },                   -- table: additional groups that should be cleared
-                exclude_groups = {}, -- table: groups you don't want to clear
-            })
-        end
-    },
+    -- {
+    --     'xiyaowong/transparent.nvim',
+    --     priority = 999,
+    --     config = function()
+    --         require('transparent').setup({
+    --             groups = { -- table: default groups
+    --                 'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+    --                 'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+    --                 'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+    --                 'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+    --             },
+    --             extra_groups = {
+    --                 "NormalFloat",   -- plugins which have float panel such as Lazy, Mason, LspInfo
+    --                 "NvimTreeNormal" -- NvimTree
+    --             },                   -- table: additional groups that should be cleared
+    --             exclude_groups = {}, -- table: groups you don't want to clear
+    --         })
+    --     end
+    -- },
 }
